2f74695d273dde46d202b40162cc6ea62747ead5
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2019-11-13 16:07:52 +0545

    Add files via upload

--- blog/products/forms.py
+++ blog/products/forms.py
@@ -2,6 +2,19 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+	title=forms.CharField(required=True)
+	description=forms.CharField(label="Info",
+			 widget=forms.Textarea(
+			 	attrs={
+			 	"rows":12,
+			 	"cols":14,
+			 	"placeholder":"your description"
+			 	}
+			 	),
+			 required=False
+			 )
+	email=forms.EmailField(label="Enter Your email")
+	price=forms.DecimalField(initial=199)
 	class Meta:
 		model= Product
 		fields=[
@@ -9,4 +22,30 @@ class ProductForm(forms.ModelForm):
 			'description',
 			'price',
 			'summary',
+			
 		]
+	def clean_title(self,*args,**kwargs):
+		title=self.cleaned_data.get("title")
+		if "nir" in title:
+			return title
+		else:
+			raise forms.ValidationError("not a valid title")
+def clean_emails(self,*args,**kwargs):
+		email=self.cleaned_data.get("email")
+		if "nir" in email:
+			return email
+		else:
+			raise forms.ValidationError("not a valid title")
+class RawProductForm(forms.Form):
+			title=forms.CharField(required=True)
+			description=forms.CharField(label="Info",
+			 widget=forms.Textarea(
+			 	attrs={
+			 	"rows":12,
+			 	"cols":14,
+			 	"placeholder":"your description"
+			 	}
+			 	),
+			 required=False
+			 )
+			price=forms.DecimalField(initial=199)
--- blog/products/views.py
+++ blog/products/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from .forms import ProductForm
+from .forms import ProductForm , RawProductForm
 from .models import Product
 # Create your views here.
 def product_create_view(request):
-	context={
-	}
-	return render
+    # my_form=ProductForm()
+    # if request.method == "POST":
+    #     my_form=ProductForm(request.POST)
+    #     if my_form.is_valid():
+    #      print(my_form.cleaned_data)
+    #      Product.objects.create(**my_form.cleaned_data)
+    #     else:
+    #      print(my_form.errors)
+    # context={
+    # "form":my_form
+    # }
+    return render(request,"product_create.html",context)
 def product_detail_view(request):
-	obj=Product.objects.get(id=1)
-	context={
-	'object':obj
-	}
-	return render(request,"product_detail.html",context)
+    obj=Product.objects.get(id=1)
+    context={
+    'object':obj
+    }
+    return render(request,"product_detail.html",context)
